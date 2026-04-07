47152be62c1c83f8b5b04bfcc88514c7d68dac65
Aissam Azougagh <issamazougagh7@gmail.com>
2026-04-07 17:43:04 +0100

    Implement JWT Authentication

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// --- الربط مع الداتابيز ---
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("✅ Connected to MongoDB"))
+    .catch(err => console.log("❌ DB Error:", err));
+
+// --- خلي هاد السطر فيه // فالبداية حيت باقي ما كاينش هاد المجلد ---
+// app.use('/api/users', require('./routes/userRoutes'));
+
+app.get('/', (req, res) => res.send("API is working!"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server on port ${PORT}`));
